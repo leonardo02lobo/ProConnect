b519b6e0a29f08f99cb3265272aa326a64c5927e
leonardo lobo <loboleonardo75@gmail.com>
2025-03-25 20:17:31 -0400

    Intento de publicacion

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://leonardo02lobo.github.io',
+    base: 'ProConnect',
     vite: {
         plugins: [tailwindcss()],
     }
